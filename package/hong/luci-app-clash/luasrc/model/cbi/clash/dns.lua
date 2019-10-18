feb8b377e33f2d05dc45fcc688edcf6591fcc312
hong0980 <hong0980@qq.com>
2019-10-18 21:03:51 +0800

    up

--- package/hong/luci-app-clash/luasrc/model/cbi/clash/dns.lua
+++ package/hong/luci-app-clash/luasrc/model/cbi/clash/dns.lua
@@ -8,6 +8,7 @@ local uci = require("luci.model.uci").cursor()
 
 m = Map("clash")
 s = m:section(TypedSection, "clash")
+--m.pageaction = false
 s.anonymous = true
 s.addremove=false
 
@@ -28,7 +29,7 @@ md.description = translate("Enabling Custom DNS will Overwrite your config.yaml 
 local dns = "/usr/share/clash/dns.yaml"
 o = s:option(TextValue, "dns",translate("Modify yaml DNS"))
 o.template = "clash/tvalue"
-o.rows = 25
+o.rows = 26
 o.wrap = "off"
 o.cfgvalue = function(self, section)
 	return NXFS.readfile(dns) or ""
@@ -38,6 +39,20 @@ o.write = function(self, section, value)
 end
 o.description = translate("NB: press ENTER to create a blank line at the end of input.")
 o:depends("mode", 1)
+
+
+local clash_conf = "/etc/clash/config.yaml"
+local apply = luci.http.formvalue("cbi.apply")
+if apply then
+if NXFS.access(clash_conf) then
+	uci:commit("clash")
+	SYS.call("sh /usr/share/clash/yum_change.sh 2>&1 &")
+	if luci.sys.call("pidof clash >/dev/null") == 0 then
+	SYS.call("/etc/init.d/clash restart >/dev/null 2>&1 &")
+	end
+end
+end
+
 
 return m
 
--- package/hong/luci-app-clash/root/usr/share/clash/clash.sh
+++ package/hong/luci-app-clash/root/usr/share/clash/clash.sh
@@ -1,42 +1,22 @@
-#!/bin/bash
-subscribe_url=$(uci get clash.config.subscribe_url 2>/dev/null)
+#!/bin/sh /etc/rc.common
+subscribe_url=$(uci get clash.config.subscribe_url_clash 2>/dev/null)
 subtype=$(uci get clash.config.subcri 2>/dev/null)
 urlv2ray=$(uci get clash.config.v2ray 2>/dev/null)
-urlsurge=$(uci get clash.config.surge 2>/dev/null)
-CONFIG_YAML="/etc/clash/config.yaml"
-CONFIG_YAML_TEMP="/etc/clash/server.yaml"
-CONFIG_YAML_RULE="/usr/share/clash/rule.yaml"
-if pidof clash >/dev/null; then
-/etc/init.d/clash stop 2>/dev/null
-rm -rf /etc/clash/config.bak 2> /dev/null
-if [ $subtype == "clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $subscribe_url -O 2>&1 >1 $CONFIG_YAML
-elif [ $subtype == "v2rayn2clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $urlv2ray.$subscribe_url -O 2>&1 >1 $CONFIG_YAML_TEMP
-if [ -f $CONFIG_YAML_TEMP ];then
-sed -i '/Rule:/,$d' $CONFIG_YAML_TEMP 
-cat $CONFIG_YAML_TEMP $CONFIG_YAML_RULE > $CONFIG_YAML 
-fi
-elif [ $subtype == "surge2clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $urlsurge.$subscribe_url -O 2>&1 >1 $CONFIG_YAML
-fi
-rm -rf $CONFIG_YAML_TEMP 2> /dev/null
-uci set clash.config.enable=1 2> /dev/null
-uci commit clash 2> /dev/null
-/etc/init.d/clash restart 2>/dev/null
-else
-rm -rf /etc/clash/config.bak 2> /dev/null
-if [ $subtype == "clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $subscribe_url -O 2>&1 >1 $CONFIG_YAML
-elif [ $subtype == "v2rayn2clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $urlv2ray.$subscribe_url -O 2>&1 >1 $CONFIG_YAML_TEMP
-if [ -f $CONFIG_YAML_TEMP ];then
-sed -i '/Rule:/,$d' $CONFIG_YAML_TEMP 
-cat $CONFIG_YAML_TEMP $CONFIG_YAML_RULE > $CONFIG_YAML 
-fi
-elif [ $subtype == "surge2clash" ];then
-wget-ssl --timeout=30 --tries=2 --user-agent="User-Agent: Mozilla" $urlsurge.$subscribe_url -O 2>&1 >1 $CONFIG_YAML
-fi
-rm -rf $CONFIG_YAML_TEMP 2> /dev/null
-fi
+urlssr=$(uci get clash.config.ssr 2>/dev/null)
+config_type=$(uci get clash.config.config_type 2>/dev/null)
+
+CONFIG_YAML="/usr/share/clash/config/sub/config.yaml" 
+
+	if pidof clash >/dev/null; then
+		if [ $subtype == "clash" ];then
+			wget-ssl --no-check-certificate --user-agent="Clash/OpenWRT" $subscribe_url -O 2>&1 >1 $CONFIG_YAML
+		fi
+		if [ $config_type == "sub" ];then 
+		/etc/init.d/clash restart 2>/dev/null
+		fi
+	else
+		if [ $subtype == "clash" ];then
+			wget-ssl --no-check-certificate --user-agent="Clash/OpenWRT" $subscribe_url -O 2>&1 >1 $CONFIG_YAML
+		fi
+	fi
 
