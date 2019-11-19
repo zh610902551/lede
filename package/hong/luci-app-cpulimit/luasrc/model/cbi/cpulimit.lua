5b1a8e503b4c194561e143f5a22965c6432a6087
hong0980 <hong0980@qq.com>
2019-11-19 19:25:10 +0800

    up

--- package/hong/luci-app-cpulimit/luasrc/model/cbi/cpulimit.lua
+++ package/hong/luci-app-cpulimit/luasrc/model/cbi/cpulimit.lua
@@ -1,4 +1,4 @@
-m=Map("cpulimit",translate("cpulimit"),translate("Use cpulimit to restrict app's cpu usage."))
+m=Map("cpulimit",translate("cpulimit"),translate("Use cpulimit to limit app's cpu using."))
 s=m:section(TypedSection,"list",translate("Settings"))
 s.template="cbi/tblsection"
 s.anonymous=true
@@ -8,7 +8,7 @@ enable.optional=false
 enable.rmempty=false
 local e="ps | awk '{print $5}' | sed '1d' | sort -k2n | uniq | sed '/^\\\[/d' | sed '/sed/d' | sed '/awk/d' | sed '/hostapd/d' | sed '/pppd/d' | sed '/mwan3/d' | sed '/sleep/d' | sed '/sort/d' | sed '/ps/d' | sed '/uniq/d' | awk -F '/' '{print $NF}'"
 local e=io.popen(e,"r")
-exename=s:option(Value,"exename",translate("exename"),translate("name of the executable program file or path name"))
+exename=s:option(Value,"exename",translate("exename"),translate("name of the executable program file.CAN NOT BE A PATH!"))
 exename.optional=false
 exename.rmempty=false
 exename.default="vsftpd"
