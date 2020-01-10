4c45c3cb960b784c1e5d62a450e2400f7b6a01a6
hong0980 <hong0980@qq.com>
2020-01-10 15:51:46 +0800

    up

--- include/target.mk
+++ include/target.mk
@@ -16,9 +16,9 @@ DEVICE_TYPE?=router
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd block-mount coremark \
 kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget \
 default-settings luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-adbyby-plus luci-app-autoreboot \
-luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-guest-wifi luci-app-cpulimit luci-app-ttyd luci-app-smartinfo luci-app-zerotier luci-app-syncdial luci-app-wifischedule \
+luci-app-filetransfer luci-app-vsftpd luci-app-guest-wifi luci-app-cpulimit luci-app-ttyd luci-app-smartinfo luci-app-zerotier luci-app-syncdial luci-app-wifischedule \
 luci-app-pptp-server luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree luci-app-appfilter  luci-app-serverchan luci-app-softwarecenter \
-luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol 
+luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol luci-app-passwall
 # For nas targets
 DEFAULT_PACKAGES.nas:=fdisk lsblk mdadm automount autosamba luci-app-usb-printer 
 # For router targets
